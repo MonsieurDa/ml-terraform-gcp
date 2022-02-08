a9a28cff3f0624e7d5dbbf364b49cec61b386245
MonsieurDa <djibrillasaley@yahoo.fr>
2022-02-08 15:53:46 +0100

    Update main output.tf

--- infra/environments/dv/main.tf
+++ infra/environments/dv/main.tf
@@ -10,10 +10,19 @@ locals {
 
 module "cloudrun" {
   source           = "../../modules/cloudrun"
-  suffix           = local.suffix
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "gce-module"
-  network_tags     = ["http-server", "https-server"]
+  # suffix           = local.suffix
+  project_id   = var.gcp_project_id
+  # vpc_network_name = "default"
+  # instance_name    = "gce-module"
+  # network_tags     = ["http-server", "https-server"]
+
+
+
+  # Required variables
+  # service_name           = "<SERVICE NAME>"
+  # location               = "<LOCATION>"
+  # image                  = "gcr.io/cloudrun/hello"
+
+
 }
 
