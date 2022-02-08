5ae9394bc09b470ac31c93c75b6b2f46d48b66d7
MonsieurDa <djibrillasaley@yahoo.fr>
2022-02-08 17:42:37 +0100

    Enable api's

--- infra/environments/dv/main.tf
+++ infra/environments/dv/main.tf
@@ -1,5 +1,5 @@
 resource "google_project_service" "enable_cloud_resource_manager_api" {
-  services = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com"]
+  service = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com"]
   disable_dependent_services = true
 }
 
