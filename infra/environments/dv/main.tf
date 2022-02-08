e2e0c8fa0ad48052abcd239710ad106b75dd80c7
MonsieurDa <djibrillasaley@yahoo.fr>
2022-02-08 15:17:01 +0100

    Del output.tf

--- infra/environments/dv/main.tf
+++ infra/environments/dv/main.tf
@@ -8,12 +8,10 @@ locals {
   suffix = format("%s-%s", "tf", random_string.launch_id.result)
 }
 
-module "gce-module" {
+module "cloudrun" {
   source           = "../../modules/cloudrun"
   suffix           = local.suffix
   gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "gce-module"
-  network_tags     = ["http-server", "https-server"]
+  
 }
 
