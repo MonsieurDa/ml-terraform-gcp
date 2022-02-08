5d6d81ec88fbb1f1f2ce0fc2e8e66e894178d4eb
MonsieurDa <djibrillasaley@yahoo.fr>
2022-02-08 16:51:02 +0100

    Update env ymal file

--- infra/environments/dv/main.tf
+++ infra/environments/dv/main.tf
@@ -6,7 +6,7 @@ resource "random_string" "launch_id" {
 
 module "cloudrun" {
   source           = "../../modules/cloudrun"
-  poject_id   = var.gcp_project_id
+  poject_id              = var.gcp_project_id
   service_name           = var.service_name
   location               = var.location
   image                  = var.image
