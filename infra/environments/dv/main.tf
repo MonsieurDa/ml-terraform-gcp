4c4d1e2469427cd2fe28010ba7f168944685ac72
MonsieurDa <djibrillasaley@yahoo.fr>
2022-02-16 15:03:04 +0100

    Del _README.md

--- infra/environments/dv/main.tf
+++ infra/environments/dv/main.tf
@@ -3,7 +3,6 @@ resource "google_project_service" "gcp_cr" {
   service = "run.googleapis.com"
 }
 
-
 resource "google_project_service" "crm_api" {
   service = "cloudresourcemanager.googleapis.com"
 }
@@ -11,6 +10,7 @@ resource "google_project_service" "crm_api" {
 resource "google_project_service" "iam_api" {
   service = "iam.googleapis.com"
 }
+
 
 # modules
 module "cloudrun" {
